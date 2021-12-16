c9debff1147f5724b2ba9888b9234ea4e9230980
Woradol555 <oakthegang79@gmail.com>
2021-12-16 18:56:42 +0700

    editfile

--- welcome_message.php
+++ welcome_message.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
 
-	<!-- STYLES -->
+	
 
 	<style {csp-style-nonce}>
 		* {
